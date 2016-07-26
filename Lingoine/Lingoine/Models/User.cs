83ef0f6ffb10be963f52d46b99bee42a8d82d6d9
Ranveer Aggarwal <ranveeraggarwal.msft@gmail.com>
2016-07-26 15:12:24 +0530

    Stuff

--- Lingoine/Lingoine/Models/User.cs
+++ Lingoine/Lingoine/Models/User.cs
@@ -8,17 +8,18 @@ namespace Lingoine.Models
 {
     public class User
     {
-        public int Id { get; set; }
         public string Username { get; set; }
-        public int Age { get; set; }
-        public string Place { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string State { get; set; }
         public string Country { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public string SkypeId { get; set; }
         public bool IsPremium { get; set; }
-        public DateTime DateOfBirth { get; set; }
         public bool Gender { get; set; }
+        public bool IsOnline { get; set; }
+        public bool IsBusy { get; set; }
+
     }
 }
 
--- Lingoine/Lingoine/Views/SignUp.xaml.cs
+++ Lingoine/Lingoine/Views/SignUp.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Lingoine.Models;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,17 +29,50 @@ namespace Lingoine.Views
 
         private void submitButton_Click(object sender, RoutedEventArgs e)
         {
-
+            bool gender = true;
+            if (radioButtonMale.IsChecked == true)
+            {
+                gender = true;
+            }
+            else if (radioButtonFemale.IsChecked == true)
+            {
+                gender = false;
+            } 
+            var client = new RestClient("http://requestb.in/1ced5701");
+            var request = new RestRequest("", Method.POST);
+            request.RequestFormat = RestSharp.DataFormat.Json;
+            request.AddBody(new User
+            {
+                Username = textBoxUserName.Text,
+                DateOfBirth = dateOfBirth.DisplayDate,
+                State = "State",
+                Country = "Country",
+                Email = textBoxEmail.Text,
+                Password = passwordBox1.Password,
+                SkypeId = "skypeID",
+                IsPremium = false,
+                Gender = gender,
+                IsOnline = false,
+                IsBusy = false
+            });
+            var response = client.Execute(request);
+            System.Diagnostics.Debug.WriteLine(response);
         }
 
         private void resetButton_Click(object sender, RoutedEventArgs e)
         {
-
+            textBoxUserName.Text = string.Empty;
+            dateOfBirth.DisplayDate = DateTime.Today;
+            textBoxEmail.Text = string.Empty;
+            passwordBox1.Password = string.Empty;
+            passwordBoxConfirm.Password = string.Empty;
+            radioButtonFemale.IsChecked = false;
+            radioButtonMale.IsChecked = false;
         }
 
         private void cancelButton_Click(object sender, RoutedEventArgs e)
         {
-
+            this.NavigationService.Navigate(new MainScreen());
         }
     }
 }
